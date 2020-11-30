d1e109263917961cdd6d87ed46999ea45b48a5af
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-30 20:35:18 +0530

    Practical 18

---
+++ JS/practical18.js
@@ -0,0 +1,95 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        if (input.value.trim() === "") {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not valid");
+    }
+}
+
+function checkLengthuser(input, min, max) {
+
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be atleast ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        if (/^[a-z0-9]{4,24}$/.test(input.value)) {
+            showSuccess(input);
+        } else {
+            showError(
+                input,
+                `${getFieldName(input)} must be lowecase without whitespaces`
+            );
+        }
+    }
+}
+
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be atleast ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPassword(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Passwords do not match");
+    }
+}
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    checkRequired([username, email, password1, password2]);
+    checkLengthuser(username, 3, 15);
+    checkLength(password1, 6, 20);
+    checkEmail(email);
+    checkPassword(password1, password2);
+});
